143fd63fa33c258079fb6a913d255a97dadc864e
Carlos Aquino <carlos.aquino.ext@infracommerce.com.br>
2022-09-30 11:41:26 -0300

    nx docker error

---
+++ apps/apis/api-testes/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'api-testes',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/apps/apis/api-testes',
+};
